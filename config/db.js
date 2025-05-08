99003e5f6167149673d33092c5a7b121904cb3d8
AlishanMasood <alishanmasood20@gmail.com>
2025-05-08 22:41:29 +0500

    added git ignore

---
+++ config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const dbConnection = await mongoose.connect(process.env.DB_URL);
+    console.log(`✅ MongoDB connected: ${dbConnection.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error", error.message);
+    process.exit(1);
+  }
+};
+export default connectDB;

babc03bb6cf90f5db13a286ce775b2118d7b4497
AlishanMasood <alishanmasood20@gmail.com>
2025-05-21 19:55:59 +0500

    created server and added express.json() for payloads in req body

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import router from "./routes/index.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
+app.use("/api", router);
 app.get("/", (req, res) => {
   res.send("Hello World! on top");
 });
@@ -13,7 +16,7 @@ app.get("/", (req, res) => {
 function main() {
   connectDB();
   app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`);
+    console.log(`Example app listening on port 😆 ${process.env.PORT}`);
   });
 }
 
